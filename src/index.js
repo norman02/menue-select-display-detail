a4578edfc84a192e19be68615810abb6e72eb04f
John N. Norman <normanj777@gmail.com>
2021-02-19 07:42:58 -0500

    prototype app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './css/main.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const questions = [
+    {
+        question: 'How many team members can I invite?',
+        answer: 'You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.',
+    },
+    {
+        question: 'What is the maximum file upload size?',
+        answer: 'No more than 2GB. All files in your account must fit your allotted storage space.',
+    },
+    {
+        question: 'How do I reset my password?',
+        answer: 'Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.',
+    },
+    {
+        question: 'Can I cancel my subscription?',
+        answer: 'Yes! Send us a message and we’ll process your request no questions asked.',
+    },
+    {
+        question: 'Do you provide additional support?',
+        answer: 'Chat and email support is available 24/7. Phone lines are open during normal business hours.',
+    },
+]
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const faq = questions.map((question) => {
+    return (
+        <li>
+            <button>{question.question}</button>
+            <p>{question.answer}</p>
+        </li>
+    )
+})
+
+class App extends React.Component {
+
+
+    render() {
+        return (
+            <main>
+            <h1>Frequently Asked Questions</h1>
+            <ul>
+                {faq}
+            </ul>
+    
+        </main>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
